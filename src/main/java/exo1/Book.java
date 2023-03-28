1ec11c78159d292ba5d7c5853e2dc6e0dc7dbdff
ralixt <98614258+ralixt@users.noreply.github.com>
2023-03-28 14:47:13 +0200

    add Book.java

---
+++ src/main/java/exo1/Book.java
@@ -0,0 +1,39 @@
+package exo1;
+
+import java.util.Date;
+
+public class Book {
+    private String title;
+    private String author;
+    private Date date;
+
+    public Book(String title, String author, Date date) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
