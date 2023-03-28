3d757dad743fc293b95bf3f365de20f5edd846a3
ralixt <98614258+ralixt@users.noreply.github.com>
2023-03-28 15:58:52 +0200

    add MethodSource

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,7 +7,5 @@ L
 src/main/java/bank/Bank.java,4\f\4f6cd818acc7047cd7712a40b17e3d9bb1f5c367
 L
 src/main/java/book/Book.java,b\e\be4f461a775d1ae400fb1579182a8f713e68b0ac
-P
- src/test/java/book/BookTest.java,8\a\8aca1cda87d0f6199ab3d0e10fecf5e56b5d4e63
 O
 src/main/java/bank/Account.java,1\6\161eed6c79810259debe54240341b1e5515b928d
--- src/test/java/book/BookTest.java
+++ src/test/java/book/BookTest.java
@@ -2,44 +2,45 @@ package book;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
-    @Test
+    @MethodSource
     void getTitle() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         Assertions.assertEquals("Les fourberies de Scapin", book.getTitle());
     }
 
-    @Test
+    @MethodSource
     void setTitle() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         book.setTitle("Ok");
         Assertions.assertEquals("Ok", book.getTitle());
     }
 
-    @Test
+    @MethodSource
     void getAuthor() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         Assertions.assertEquals("Molière", book.getAuthor());
     }
 
-    @Test
+    @MethodSource
     void setAuthor() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         book.setAuthor("Ok");
         Assertions.assertEquals("Ok", book.getAuthor());
     }
 
-    @Test
+    @MethodSource
     void getDate() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         Assertions.assertEquals("1671", book.getDate());
     }
 
-    @Test
+    @MethodSource
     void setDate() {
         Book book = new Book("Les fourberies de Scapin", "Molière", "1671");
         book.setDate("1500");
